6dd77f4bb7807a9d3fba2fc9eb96c7ad76f05250
DeepakRaj-Jaganathan <deepakthipu228@gmail.com>
2020-10-26 14:22:34 +0530

    add endpoint file

--- src/test/java/com/automation/zomatoapi/ZomatoEstablishmentApiTest.java
+++ src/test/java/com/automation/zomatoapi/ZomatoEstablishmentApiTest.java
@@ -1,18 +1,19 @@
 package com.automation.zomatoapi;
 
+import apiEngine.EndPoints;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class ZomatoEstablishmentApiTest extends BaseSetup {
+public class ZomatoEstablishmentApiTest {
 
     @Test
     public void testGetEstablishmentsWithValidCityId(){
         int city_id = 7;
         Response resp = RestAssured.given()
-                .spec(getBaseRequestSpecification()).queryParam("city_id",city_id).get("/establishments");
+                .spec(EndPoints.getBaseRequestSpecification()).queryParam("city_id",city_id).get(EndPoints.establishments());
         JSONObject jsonobj = new JSONObject(resp.body().asString());
         Assert.assertEquals(resp.getStatusCode(),200);
         Assert.assertNotNull(jsonobj.get("establishments"),"No establishments in this region, please check.");
@@ -22,7 +23,7 @@ public class ZomatoEstablishmentApiTest extends BaseSetup {
     public void testGetEstablishmentsWithCityIdAsZero(){
         int city_id = 0;
         Response resp = RestAssured.given()
-                .spec(getBaseRequestSpecification()).queryParam("city_id",city_id).get("/establishments");
+                .spec(EndPoints.getBaseRequestSpecification()).queryParam("city_id",city_id).get(EndPoints.establishments());
         Assert.assertEquals(resp.getStatusCode(),400);
         JSONObject jsonobj = new JSONObject(resp.body().asString());
         Assert.assertEquals(jsonobj.get("message").toString(),"Invalid or missing parameter \"city_id\"");
@@ -32,7 +33,7 @@ public class ZomatoEstablishmentApiTest extends BaseSetup {
     public void testGetEstablishmentsWithInValidCityId(){
         int city_id = -86;
         Response resp = RestAssured.given()
-                .spec(getBaseRequestSpecification()).queryParam("city_id",city_id).get("/establishments");
+                .spec(EndPoints.getBaseRequestSpecification()).queryParam("city_id",city_id).get(EndPoints.establishments());
         Assert.assertEquals(resp.getStatusCode(),400);
         JSONObject jsonobj = new JSONObject(resp.body().asString());
         Assert.assertEquals(jsonobj.get("message").toString(),"Invalid or missing parameter \"city_id\"");
@@ -42,7 +43,7 @@ public class ZomatoEstablishmentApiTest extends BaseSetup {
     public void testGetEstablishmentsWithLatOnly(){
         double latitude = 13.0827;
         Response resp = RestAssured.given()
-                .spec(getBaseRequestSpecification()).queryParam("lat",latitude).get("/establishments");
+                .spec(EndPoints.getBaseRequestSpecification()).queryParam("lat",latitude).get(EndPoints.establishments());
         JSONObject jsonobj = new JSONObject(resp.body().asString());
         Assert.assertEquals(resp.getStatusCode(),400);
         Assert.assertEquals(jsonobj.get("message").toString(),"Invalid or missing parameter \"city_id\"");
@@ -52,7 +53,7 @@ public class ZomatoEstablishmentApiTest extends BaseSetup {
     public void testGetEstablishmentsWithLonOnly(){
         double longitude = 80.2707;
         Response resp = RestAssured.given()
-                .spec(getBaseRequestSpecification()).queryParam("lon",longitude).get("/establishments");
+                .spec(EndPoints.getBaseRequestSpecification()).queryParam("lon",longitude).get(EndPoints.establishments());
         JSONObject jsonobj = new JSONObject(resp.body().asString());
         Assert.assertEquals(resp.getStatusCode(),400);
         Assert.assertEquals(jsonobj.get("message").toString(),"Invalid or missing parameter \"city_id\"");
@@ -63,8 +64,8 @@ public class ZomatoEstablishmentApiTest extends BaseSetup {
         double latitude = 13.0827;
         double longitude = 80.2707;
         Response resp = RestAssured.given()
-                .spec(getBaseRequestSpecification()).queryParam("lat",latitude).
-                        queryParam("lon",longitude).get("/establishments");
+                .spec(EndPoints.getBaseRequestSpecification()).queryParam("lat",latitude).
+                        queryParam("lon",longitude).get(EndPoints.establishments());
         Assert.assertEquals(resp.getStatusCode(),200);
     }
 
@@ -74,8 +75,8 @@ public class ZomatoEstablishmentApiTest extends BaseSetup {
         double longitude = 80.2707;
         int city_id = 7;
         Response resp = RestAssured.given()
-                .spec(getBaseRequestSpecification()).queryParam("city_id",city_id).queryParam("lat",latitude).
-                        queryParam("lon",longitude).get("/establishments");
+                .spec(EndPoints.getBaseRequestSpecification()).queryParam("city_id",city_id).queryParam("lat",latitude).
+                        queryParam("lon",longitude).get(EndPoints.establishments());
         JSONObject jsonobj = new JSONObject(resp.body().asString());
         Assert.assertNotNull(jsonobj.get("establishments"),"No establishments in this region, please check.");
         Assert.assertEquals(resp.getStatusCode(),200);
@@ -87,8 +88,8 @@ public class ZomatoEstablishmentApiTest extends BaseSetup {
         double longitude = 77.5946;
         int city_id = 7;
         Response resp = RestAssured.given()
-                .spec(getBaseRequestSpecification()).queryParam("city_id",city_id).queryParam("lat",latitude).
-                        queryParam("lon",longitude).get("/establishments");
+                .spec(EndPoints.getBaseRequestSpecification()).queryParam("city_id",city_id).queryParam("lat",latitude).
+                        queryParam("lon",longitude).get(EndPoints.establishments());
         JSONObject jsonobj = new JSONObject(resp.body().asString());
         Assert.assertNotNull(jsonobj.get("establishments"),"No establishments in this region, please check.");
         Assert.assertEquals(resp.getStatusCode(),200);
